59be5c511a41f6df49e0ad42db405eaf6bd2dd9f
Manfred Schmid <Manfred@klangobjekte.com>
2013-03-03 19:24:23 +0100

    user rights

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 #include "QSettings"
 #include "QDebug"
+#include "QProcess"
+#include "QFileInfo"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -21,13 +23,34 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_deletePrefsPushButton_Clicked(){
 
-    //QSettings settings;
+
+
+
     QSettings settings("KlangObjekte.", "ProSoundFinder2");
     //settings.fileName();
     qDebug()<< "settings: " << settings.fileName();
     //"\HKEY_CURRENT_USER\Software\KlangObjekte\ProSoundFinder2"
+    QProcess process;
+    QStringList argumentStringList;
+    argumentStringList.append("777");
+    argumentStringList.append(settings.fileName());
+    QFileInfo info(settings.fileName());
+    if(info.exists())
+    {
+    bool success = process.startDetached("chmod",argumentStringList);
+    process.close();
+    qDebug() << "chmod" << settings.fileName()  << success;
     settings.clear();
     settings.deleteLater();
     ui->label->setText("Preferences Deleted!");
+
+    }
+    else{
+        qDebug() << "no Preferences Found";
+    ui->label->setText("no Preferences Found!");
+    }
+
+
+
 
 }
